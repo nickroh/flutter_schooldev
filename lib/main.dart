7588fecbba6977bc2378a590842c0a73e4e4c25c
Roh Geonho <roh517@naver.com>
2019-09-30 15:14:59 +0900

    login complete
    https://medium.com/flutterpub/flutter-how-to-do-user-login-with-firebase-a6af760b14d5

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
-import 'package:flutter_schooldev/setup/pages/welcome.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_schooldev/services/authentication.dart';
+import 'package:flutter_schooldev/pages/root_page.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: WelcomePage(),
-    );
+    return new MaterialApp(
+        title: 'Flutter login demo',
+        debugShowCheckedModeBanner: false,
+        theme: new ThemeData(
+          primarySwatch: Colors.lightGreen,
+        ),
+        home: new RootPage(auth: new Auth()));
   }
 }
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_schooldev/services/authentication.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:async';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.auth, this.userId, this.onSignedOut})
+      : super(key: key);
+
+  final BaseAuth auth;
+  final VoidCallback onSignedOut;
+  final String userId;
+
+  @override
+  State<StatefulWidget> createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  final _textEditingController = TextEditingController();
+
+
+  Query _todoQuery;
+
+  bool _isEmailVerified = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _checkEmailVerification();
+
+      }
+
+  void _checkEmailVerification() async {
+    _isEmailVerified = await widget.auth.isEmailVerified();
+    if (!_isEmailVerified) {
+      _showVerifyEmailDialog();
+    }
+  }
+
+  void _resentVerifyEmail(){
+    widget.auth.sendEmailVerification();
+    _showVerifyEmailSentDialog();
+  }
+
+  void _showVerifyEmailDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("Verify your account"),
+          content: new Text("Please verify account in the link sent to email"),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text("Resent link"),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _resentVerifyEmail();
+              },
+            ),
+            new FlatButton(
+              child: new Text("Dismiss"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showVerifyEmailSentDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("Verify your account"),
+          content: new Text("Link to verify account has been sent to your email"),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text("Dismiss"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+
+
+
+
+
+
+  _signOut() async {
+    try {
+      await widget.auth.signOut();
+      widget.onSignedOut();
+    } catch (e) {
+      print(e);
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('SchoolDev'),
+          actions: <Widget>[
+            new FlatButton(
+                child: new Text('Logout',
+                    style: new TextStyle(fontSize: 17.0, color: Colors.white)),
+                onPressed: _signOut)
+          ],
+        ),
+    );
+  }
+}
