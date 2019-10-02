3ca515e1107efc27a09acbbe58613339b3c4fc64
Roh Geonho <roh517@naver.com>
2019-10-03 00:13:16 +0900

    post create (begin)

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-        title: 'Flutter login demo',
+        title: 'SchoolDev',
         debugShowCheckedModeBanner: false,
         theme: new ThemeData(
           primarySwatch: Colors.lightGreen,
