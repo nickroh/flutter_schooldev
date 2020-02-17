218135f937ad6b48ba2880d6c898089cca25971a
Roh Geonho <roh517@naver.com>
2020-02-17 16:04:13 +0900

    android studio err

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -3,78 +3,18 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
-
-#if __has_include(<cloud_firestore/CloudFirestorePlugin.h>)
 #import <cloud_firestore/CloudFirestorePlugin.h>
-#else
-@import cloud_firestore;
-#endif
-
-#if __has_include(<dynamic_list_view/DynamicListViewPlugin.h>)
 #import <dynamic_list_view/DynamicListViewPlugin.h>
-#else
-@import dynamic_list_view;
-#endif
-
-#if __has_include(<firebase_auth/FirebaseAuthPlugin.h>)
 #import <firebase_auth/FirebaseAuthPlugin.h>
-#else
-@import firebase_auth;
-#endif
-
-#if __has_include(<firebase_core/FLTFirebaseCorePlugin.h>)
-#import <firebase_core/FLTFirebaseCorePlugin.h>
-#else
-@import firebase_core;
-#endif
-
-#if __has_include(<firebase_storage/FLTFirebaseStoragePlugin.h>)
-#import <firebase_storage/FLTFirebaseStoragePlugin.h>
-#else
-@import firebase_storage;
-#endif
-
-#if __has_include(<flutter_appavailability/AppAvailability.h>)
+#import <firebase_core/FirebaseCorePlugin.h>
+#import <firebase_storage/FirebaseStoragePlugin.h>
 #import <flutter_appavailability/AppAvailability.h>
-#else
-@import flutter_appavailability;
-#endif
-
-#if __has_include(<flutter_launch/FlutterLaunchPlugin.h>)
 #import <flutter_launch/FlutterLaunchPlugin.h>
-#else
-@import flutter_launch;
-#endif
-
-#if __has_include(<google_sign_in/FLTGoogleSignInPlugin.h>)
-#import <google_sign_in/FLTGoogleSignInPlugin.h>
-#else
-@import google_sign_in;
-#endif
-
-#if __has_include(<intro_slider/IntroSliderPlugin.h>)
+#import <google_sign_in/GoogleSignInPlugin.h>
 #import <intro_slider/IntroSliderPlugin.h>
-#else
-@import intro_slider;
-#endif
-
-#if __has_include(<share/FLTSharePlugin.h>)
-#import <share/FLTSharePlugin.h>
-#else
-@import share;
-#endif
-
-#if __has_include(<sqflite/SqflitePlugin.h>)
+#import <share/SharePlugin.h>
 #import <sqflite/SqflitePlugin.h>
-#else
-@import sqflite;
-#endif
-
-#if __has_include(<url_launcher/FLTURLLauncherPlugin.h>)
-#import <url_launcher/FLTURLLauncherPlugin.h>
-#else
-@import url_launcher;
-#endif
+#import <url_launcher/UrlLauncherPlugin.h>
 
 @implementation GeneratedPluginRegistrant
 
@@ -90,7 +30,7 @@
   [IntroSliderPlugin registerWithRegistrar:[registry registrarForPlugin:@"IntroSliderPlugin"]];
   [FLTSharePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharePlugin"]];
   [SqflitePlugin registerWithRegistrar:[registry registrarForPlugin:@"SqflitePlugin"]];
-  [FLTURLLauncherPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTURLLauncherPlugin"]];
+  [FLTUrlLauncherPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTUrlLauncherPlugin"]];
 }
 
 @end
